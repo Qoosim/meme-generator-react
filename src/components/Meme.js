5e5dc10c2d237f90b7ad66b604ea7d42ea1f6b0a
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-12-03 21:59:58 +0100

    Add state to change the generated url and get it displayed on the UI

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import memesData from './memesData';
 
 function Meme() {
+
+  const [memeImage, setMemeImage] = React.useState("");
+
+  let url;
+  function getMemeImage() {
+    const memeArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    url = memeArray[randomNumber].url
+    setMemeImage(url);
+  }
+
   return (
     <main className="w-75 mx-auto mt-5">
-      <div class="row">
-        <div class="col">
-          <input type="text" class="form-control" placeholder="Top text" aria-label="First name" />
+      <div className="row">
+        <div className="col">
+          <input type="text" className="form-control" placeholder="Top text" aria-label="First name" />
         </div>
-        <div class="col">
-          <input type="text" class="form-control" placeholder="bottom text" aria-label="Last name" />
+        <div className="col">
+          <input type="text" className="form-control" placeholder="bottom text" aria-label="Last name" />
         </div>
       </div>
-      <button className="btn form-control btnGet mt-3">Get a new meme image  🖼</button>
+      <button 
+        className="btn form-control btnGet mt-3" 
+        onClick={getMemeImage}>Get a new meme image  🖼
+      </button>
+      <img src={memeImage} alt="Meme" className="meme-image" />
     </main>
   )
 }
